e70fc1f3dfc460b22586362ec7aff88f3dee7411
JoshuaMadd <joshua.maddelein@student.vives.be>
2024-05-02 15:21:29 +0200

    klmqsfjdmq

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    basePath: "/snookerpocket",
+    output: "export",  // <=== enables static exports
+    reactStrictMode: true,
+  };
+  
+  module.exports = nextConfig;
+  
